2a6c9df485e53bef6ad2a613f12cf9a01c35c1da
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-05 12:05:30 +0500

    Post: Adding Validations to Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+  include ActiveModel::Validations
   has_many :comments
   belongs_to :user
   has_many :likes
   has_many :images, as: :imageable
+  validates :description, presence: true, length:{maximum: 1000}
 end
