6851200015852647cea0b6f8eb10e2b108d95c8a
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-05 20:36:12 +0500

    Adding Signup and Sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'post/index'
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'post#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  resource :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
