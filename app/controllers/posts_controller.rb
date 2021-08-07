7b5db259bf1c817c8699a4b1e6ef820d967df397
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-07 18:40:53 +0500

    Adding edit post feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -21,9 +22,39 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
-
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    post_obj_check_images = Post.new(post_params)
+    if @post.images.length + post_obj_check_images.images.length <= 10
+      if @post.update(post_params)
+        redirect_to @post
+      else
+        render 'edit'
+      end
+    else
+      @post.errors.add(:base, "Post cannot contain more than 10 images")
+      render 'edit'
+    end
+  end
+  def destroy
+    @post = post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  def delete_image_attachment
+    @image = ActiveStorage::Attachment.find(params[:id])
+    @image.purge
+    redirect_back(fallback_location: posts_path)
   end
 
   private
