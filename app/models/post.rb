b655646f455ac8e2382da3318e963db9f7800f46
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-09 18:26:45 +0500

    Adding images attribute in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   has_many :likes
-  has_many :images, as: :imageable
+  has_many_attached :images
   validates :description, presence: true, length: { maximum: 1000}
 end
