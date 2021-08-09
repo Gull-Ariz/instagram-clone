b40c1a1be462c85be9bb467a41adda584d811556
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-09 18:27:51 +0500

    Adding images attribute in story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,5 @@
 class Story < ApplicationRecord
   include ActiveModel::Validations
   belongs_to :user
-  has_many :images, as: :imageable
-  validates :image_path, presence: true
+  has_many_attached :images
 end
