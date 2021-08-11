5697cfae4ca8353d96439ce72b8f207040ded6a4
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-11 16:25:08 +0500

    Applied new ruby syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_one_attached  :profile_picture
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :user_name, presence: true, format: { with: /\A^[A-Za-z][a-zA-Z0-9]+\z/}, length: { maximum: 100}
+  validates :user_name, presence: true, format: { with: /\A^[A-Za-z][a-zA-Z0-9]+\z/ }, length: { maximum: 100 }
   validates :password, presence: true
-  validates :email, presence: true, format: { with: /\A^[A-Za-z][a-zA-Z0-9]+\z/}
+  validates :email, presence: true, format: { with: /\A^[A-Za-z][a-zA-Z0-9]+\z/ }
   end
