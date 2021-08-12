3e056fe570feb5366b88271cf3299d72c64d5b98
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-12 21:10:31 +0500

    Added comment edit and delete feature * Comments Controller: update action update comment. * Comments Controller: destroy action delete comment from post.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,26 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to authenticated_root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to authenticated_root_path
+  end
+
   private
     def comment_params
       params.require(:comment).permit( :content, :post_id )
