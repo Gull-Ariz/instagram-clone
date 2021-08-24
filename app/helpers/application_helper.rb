b0164716af86afed255be59b7ffd3a832a5556b9
GullAriz-1 <gull.ariz@devsinc.com>
2021-08-24 16:22:51 +0500

    Added suggested changes. ApplicationHelper: Improved method to get different type of collections.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def followers_suggestion
-    User.where.not(id: current_user.followeds.pluck(:user_id) << current_user.id)
+  def get_collection type
+    type.where.not(id: current_user.followeds.pluck(:user_id) << current_user.id)
   end
 end
